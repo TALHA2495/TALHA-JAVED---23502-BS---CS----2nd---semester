8b3b76cf4177f19b1d6aa95680b7528cfc38e29e
TALHA2495 <155977371+TALHA2495@users.noreply.github.com>
2024-08-31 21:59:21 +0500

    Add files via upload

---
+++ 9.5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void factorial (int n);
+int main()
+{
+int num;
+cout<<"Enter a number:";
+cin>>num;
+factorial (num);
+return 0 ;
+}
+void factorial (int n)
+{
+long fact;
+fact=1;
+for(int c=1;c<=n;c++)
+fact*=c;
+cout << fact << endl;
+ }
